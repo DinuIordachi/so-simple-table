018f25782bf6a270a0665d558f607994ee74b84a
Dinu Iordachi <dimaiordachi3@gmail.com>
2026-05-07 13:19:13 +0300

    feat(ng): re-export @sst/core types from the @sst/ng barrel

--- packages/ng/src/public-api.ts
+++ packages/ng/src/public-api.ts
@@ -1,3 +1,31 @@
+// Re-exports from @sst/core for ergonomic imports
+export type {
+	HttpQueryParams,
+	IBaseItem,
+	IColumn,
+	IColumnFilterOption,
+	IFilterParams,
+	IHttpClient,
+	IHttpRequestOptions,
+	IPaginationParams,
+	IParamFormattingStrategy,
+	IRealtimeAdapter,
+	IReadonlyObservable,
+	IRepositoryConfig,
+	IRepositoryQueryKeys,
+	IResponse,
+	IResponseError,
+	IResponseList,
+	ISortParams,
+	ITableStore,
+	ITableStoreOptions,
+	Listener,
+	ResponseListMapper,
+	Unsubscribe,
+} from '@sst/core';
+export { ESortOrder, DEFAULT_QUERY_KEYS, Observable, watch, deepEqual, arrayToMap, mapTableParams } from '@sst/core';
+
+// Angular surface
 export * from './lib/http/ng-http-client';
 export * from './lib/repositories/sst-ng-list.repository';
 export * from './lib/repositories/sst-ng-select.repository';
